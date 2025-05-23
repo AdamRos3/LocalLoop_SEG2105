07828d17eb1429d690673dfe59f9c1eb83ac2c89
Kblac0 <109545427+Kblac0@users.noreply.github.com>
2025-05-22 21:22:54 -0400

    Invalid credential handling

--- app/src/main/java/com/example/localloop/MainActivity.java
+++ app/src/main/java/com/example/localloop/MainActivity.java
@@ -1,20 +1,49 @@
 package com.example.localloop;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.activity_main);
     }
 
     public void ValidateCredentials (View view) {
-        setContentView(R.layout.activity_main);
+        // set username and password
+        EditText userText = (EditText) findViewById(R.id.username_input);
+        username = (userText).getText().toString();
+        EditText passText = (EditText) findViewById(R.id.password_input);
+        password = (passText).getText().toString();
+        //final TextView toChange = (TextView) findViewById(R.id.app_name);
+        //toChange.setText(username);
+
+        // DB query
+        HandleInvalidCredentials(view);
+    }
+
+    public void HandleInvalidCredentials (View view) {
+        // clear username and password fields
+        final TextView clearUser = (TextView) findViewById(R.id.username_input);
+        clearUser.setText("");
+        final TextView clearPass = (TextView) findViewById(R.id.password_input);
+        clearPass.setText("");
+
+        // state bad credentials
+        final TextView badCreds = (TextView) findViewById(R.id.badcreds_state);
+        badCreds.setText("Invalid credentials, please try again");
     }
 
 }
