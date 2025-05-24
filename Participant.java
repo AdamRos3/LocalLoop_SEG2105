e284df890fdf5b9aa26037fee02ccb17b3c2f5f7
Aris <aboud056@uottawa.ca>
2025-05-24 15:44:20 -0400

    test

--- Participant.java
+++ Participant.java
@@ -2,6 +2,6 @@ public class Participant extends UserAccount {
     public Participant(String inputtedUsername, String inputtedPassword) {
         super(inputtedUsername, inputtedPassword);
         typeOfAccount = "Participant";
-        //comment
+        /* comment test */
     }
 }
