818106b75ae2e85475b3994afeff9f9f7bfb3958
Aris <aboud056@uottawa.ca>
2025-05-25 17:20:34 -0400

    Updated Participant.java

--- app/src/main/java/com/example/localloop/resources/Participant.java
+++ app/src/main/java/com/example/localloop/resources/Participant.java
@@ -1,9 +1,27 @@
 package com.example.localloop.resources;
 
+import java.util.ArrayList;
+
 public class Participant extends UserAccount {
+    private ArrayList<Events> registeredEvents; // To store events that the participant registred for
     public Participant(String inputtedUsername, String inputtedPassword) {
         super(inputtedUsername, inputtedPassword);
         typeOfAccount = "Participant";
-        /* comment test */
+        registeredEvents = new ArrayList<>();
     }
+
+    //Register for an event
+    public boolean registerForEvent(Events event){
+        if(event.registerUser(getUsername())){
+            registeredEvents.add(event);
+            return true;
+        }
+        return false;
+    }
+
+    //Get list of events the user has registered for
+    public ArrayList<Events> getRegisteredEvents(){
+        return registeredEvents;
+    }
+
 }
--- app/src/main/java/com/example/localloop/resources/TestParticipant.java
+++ app/src/main/java/com/example/localloop/resources/TestParticipant.java
@@ -1,1 +1,8 @@
+package com.example.localloop.resources;
 
+public class TestParticipant {
+    public static void main(String[] args) {
+        Participant p = new Participant("username", "password");
+        System.out.println("Created participant: " + p.getUsername());
+    }
+}
