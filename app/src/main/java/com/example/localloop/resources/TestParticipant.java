c6f7dae34964768d41e80090210276045f5d722f
Aris <aboud056@uottawa.ca>
2025-05-25 17:26:57 -0400

    Updated Participant.java

--- app/src/main/java/com/example/localloop/resources/TestParticipant.java
+++ app/src/main/java/com/example/localloop/resources/TestParticipant.java
@@ -2,7 +2,8 @@ package com.example.localloop.resources;
 
 public class TestParticipant {
     public static void main(String[] args) {
-        Participant p = new Participant("username", "password");
-        System.out.println("Created participant: " + p.getUsername());
+        Participant p = new Participant("test", "123");
+        Events e = new Events("Yoga", "Relaxing", "2025-06-01", "Studio A", "Wellness", 20);
+        p.registerForEvent(e);
     }
 }
