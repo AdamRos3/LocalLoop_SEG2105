5cc5be36d1f60e633a68cdb7961116a88fcf0689
kblac098 <kblac098@uottawa.ca>
2025-07-05 18:26:44 -0400

    Update EventCategory.java

--- app/src/main/java/com/example/localloop/backend/EventCategory.java
+++ app/src/main/java/com/example/localloop/backend/EventCategory.java
@@ -4,9 +4,21 @@ public class EventCategory {
     private String name;
     private String description;
     private String categoryID;
+    public EventCategory() {
+        // Empty constructor is required by Firebase
+    }
     public EventCategory(String name, String description, String categoryID) {
         this.name = name;
         this.description = description;
         this.categoryID = categoryID;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getCategoryID() {
+        return categoryID;
     }
 }
