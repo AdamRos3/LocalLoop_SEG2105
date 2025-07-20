51085d884b3d863f4603247066880551fb6fa66e
adam <adam@rosenblatt.ca>
2025-07-20 00:19:12 -0400

    created visual cohesion between all pages

--- app/src/main/java/com/example/localloop/ui/Login.java
+++ app/src/main/java/com/example/localloop/ui/Login.java
@@ -26,7 +26,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
     public void ValidateCredentials(View view) {
